599a545a360d7b07a6cfcd684dabff96ea7c2b9b
tejus-gupta <tejusgupta14@gmail.com>
2016-09-29 16:20:50 +0530

    added segment merger function and used houghlines for line detection

--- lane_detector.cpp
+++ lane_detector.cpp
@@ -17,10 +17,11 @@ int main()
 {
 	//initialize important variables here
 	int n_segments=5;
-	int segments[5]={300, 230, 195, 120, 55};
+	int segments[5]={75, 140, 215, 250, 320};
 	Mat img_segments[5];
+	std::stringstream window_name;
 
-	cv::Mat img=cv::imread("images/l2.jpg", CV_LOAD_IMAGE_COLOR);
+	cv::Mat img=cv::imread("images/l3.jpg", CV_LOAD_IMAGE_COLOR);
 	cv::resize(img, img, cv::Size(1000,1000));
 	imshow("lanes", img);
 
@@ -42,13 +43,54 @@ int main()
 
 	extract_segments(img_segments, edges, segments, n_segments);
 
+	int i, j, k;
+
+	/*display edge segments
+	for(i=0;i<n_segments;i++)
+		cout<<img_segments[i].rows<<" "<<img_segments[i].cols<<endl;
+	
+	for(i=0;i<n_segments;i++)
+  	{
+  		window_name<<"edges"<<i<<"";
+  		imshow(window_name.str(), img_segments[i]);
+  		window_name.str("");
+  	}*/
+
 	vector<Vec4i> lines[n_segments];
+	int hough_threshold[5]={30, 30, 30, 50, 100};
+	int hough_minLineLength[5]={20, 25, 25, 30, 80};
 	for(i=0; i<n_segments ;i++)
-		HoughLinesP(lines[i], lines[i], 1, CV_PI/180, 50, 50, 10 );
+		HoughLinesP(img_segments[i], lines[i], 1, CV_PI/180, hough_threshold[i], hough_minLineLength[i], 50 );
 
-  
+  	Mat line_segments[n_segments];
+  	Mat empty=img-img;
+  	extract_segments(line_segments, empty, segments, n_segments);
 
-	imshow("edges", img);
+
+  	for(i=0;i<n_segments;i++)
+  		for(j=0;j<lines[i].size();j++)
+  		{
+  			Vec4i l = lines[i][j];
+  			line( line_segments[i], Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+  		}
+
+
+  	/* display line segments
+  	for(i=0;i<n_segments;i++)
+  	{
+  		window_name<<"window"<<i<<"";
+  		imshow(window_name.str(), line_segments[i]);
+  		window_name.str("");
+  	}*/
+
+
+  	Mat line=img-img;
+  	merge_segments(line_segments, line, segments, n_segments);
+  	imshow("detected lines",line);
+
+
+
+	imshow("edges", edges);
 	waitKey(0);
 
 	return 0;
--- lane_detector.h
+++ lane_detector.h
@@ -25,13 +25,30 @@ void extract_segments(Mat img_segments[], Mat img,int segments[], int n_segments
 	return;
 }
 
+void merge_segments(Mat img_segments[],Mat img, int segments[], int n_segments)
+{
+	int i, cum_length=0;
+	img_segments[0].copyTo(img(cv::Rect(0, 0, 1000, segments[0])));
+	cum_length+=segments[0];
+
+	for(i=1;i<=n_segments-2;i++)
+	{
+		img_segments[i].copyTo(img(cv::Rect(0, cum_length, 1000, segments[i])));
+		cum_length+=segments[i];
+	}
+
+	img_segments[n_segments-1].copyTo(img(cv::Rect(0, cum_length, 1000, 1000-cum_length)));
+	return;
+}
+
 Mat find_edges(Mat img)
 {
 	//directly in single channel
 	Mat edges;
 	cvtColor(img, img, CV_BGR2GRAY);
 	imshow("bw", img);
-	blur(img,img,Size(3,3));
+	medianBlur(img, img, 9);
+	//blur(img,img,Size(3,3));
 	Canny( img, edges, lowThreshold, highThreshold, 3 );
 
 	return edges;
